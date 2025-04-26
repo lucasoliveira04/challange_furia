b1ca68319f25707b4c8800313e46c5c10d1ed29f
Lucas Oliveira Campos <lucasolisocialmedia@gmail.com>
2025-04-26 17:25:09 -0300

    feat: serviço que utiliza a api de filtrar textos de imagens

---
+++ src/services/filter-text-image.jsx
@@ -0,0 +1,43 @@
+import { cpf } from "cpf-cnpj-validator";
+
+export const handleFilterTextInImage = async (
+  e,
+  userData,
+  setIsCpfImagemValid
+) => {
+  const file = e.target.files[0];
+
+  if (file) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/upload/", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        const filterCpf = data.texto.cpf;
+        const cleanCpf = filterCpf.replace(/\D/g, "");
+        const cpfIsValid = cpf.isValid(cleanCpf) && cleanCpf === userData.cpf;
+
+        if (cpfIsValid) {
+          console.log("CPF válido:", cleanCpf);
+          setIsCpfImagemValid(true);
+          userData.cpfImage = true;
+        } else {
+          console.log("CPF inválido ou não bateu com o userData");
+        }
+      } else {
+        setIsCpfImagemValid(false);
+        console.log(data.message);
+      }
+    } catch (e) {
+      setIsCpfImagemValid(false);
+      console.log(e);
+    }
+  }
+};
