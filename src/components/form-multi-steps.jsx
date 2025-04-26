c3220fb93555a7e1236398544c6464a43429528b
Lucas Oliveira Campos <lucasolisocialmedia@gmail.com>
2025-04-26 12:31:33 -0300

    feat: entradas de rastreamento de eventos e produtos ao formulário de várias etapas

--- src/components/form-multi-steps.jsx
+++ src/components/form-multi-steps.jsx
@@ -9,33 +9,123 @@ export const FormMultiSteps = () => {
   const [step, setStep] = useState(1);
   const [errors, setErrors] = useState({});
   const [isCpfImagemValid, setIsCpfImagemValid] = useState(false);
+  const [eventInputs, setEventInputs] = useState([{ name: "", date: "" }]);
+  const [buysLastYear, setBuysLastYear] = useState([
+    {
+      name: "",
+      value: 0,
+      date: "",
+      typeProduct: "",
+    },
+  ]);
+
+  const handleInputProductChange = (index, field, value) => {
+    const newInputs = [...buysLastYear];
+    newInputs[index][field] = value;
+    setBuysLastYear(newInputs);
+  };
+
+  const handleAddProductInput = () => {
+    const lastProduct = buysLastYear[buysLastYear.length - 1];
+
+    if (!lastProduct.name || !lastProduct.date) {
+      alert("Preencha o nome e a data do evento antes de adicionar um novo.");
+      return;
+    }
+
+    const productDate = new Date(lastProduct.date);
+    const today = new Date();
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(today.getFullYear() - 1);
+
+    if (productDate < oneYearAgo) {
+      alert("A data do evento deve ser dentro do último ano.");
+      return;
+    }
+
+    setBuysLastYear([...buysLastYear, { name: "", date: "" }]);
+  };
+
+  const handleRemoveProduct = (index) => {
+    const updateProduct = buysLastYear.filter((_, i) => i !== index);
+    setBuysLastYear(updateProduct);
+  };
+
+  const handleSaveProduct = () => {
+    updateUserData({
+      buysLastYear: buysLastYear,
+    });
+  };
+
+  const handleInputEventChange = (index, field, value) => {
+    const newInputs = [...eventInputs];
+    newInputs[index][field] = value;
+    setEventInputs(newInputs);
+  };
+
+  const handleAddEventInput = () => {
+    const lastEvent = eventInputs[eventInputs.length - 1];
+
+    if (!lastEvent.name || !lastEvent.date) {
+      alert("Preencha o nome e a data do evento antes de adicionar um novo.");
+      return;
+    }
+
+    const eventDate = new Date(lastEvent.date);
+    const today = new Date();
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(today.getFullYear() - 1);
+
+    if (eventDate < oneYearAgo) {
+      alert("A data do evento deve ser dentro do último ano.");
+      return;
+    }
+
+    setEventInputs([...eventInputs, { name: "", date: "" }]);
+  };
+
+  const handleRemoveEvent = (index) => {
+    const updateEvents = eventInputs.filter((_, i) => i !== index);
+    setEventInputs(updateEvents);
+  };
+
+  const handleSaveEvent = () => {
+    updateUserData({
+      eventsParticipationsLastYear: eventInputs,
+    });
+  };
 
   const steps = [
     {
-      label: "Nome completo",
+      label: "Qual seu nome completo?",
       name: "name",
       type: "text",
       validation: (value) => /^[A-Za-zÀ-ÿ\s]+$/.test(value),
     },
     {
-      label: "CPF",
-      name: "cpf",
-      type: "text",
-      validation: (value) => cpf.isValid(value),
-    },
-    {
-      label: "Email",
+      label: "Qual seu melhor e-mail?",
       name: "email",
       type: "email",
       validation: (value) => isEmail(value),
     },
     {
-      label: "Endereço",
+      label: "Informe seu CPF",
+      name: "cpf",
+      type: "text",
+      validation: (value) => cpf.isValid(value),
+    },
+    {
+      label: "Envie uma foto do seu CPF",
+      name: "cpfImage",
+      type: "file",
+    },
+    {
+      label: "Qual seu endereço completo?",
       name: "address",
       type: "text",
     },
     {
-      label: "Jogos de Interesse",
+      label: "Em quais jogos da FURIA você mais torce?",
       name: "interests",
       type: "button",
       options: [
@@ -44,18 +134,13 @@ export const FormMultiSteps = () => {
         "Valorant",
         "Rocket League",
         "Apex Legends",
+        "CS GO",
         "Futebol de 7",
       ],
       validation: (value) => value.length > 0,
     },
     {
-      label: "Foto do CPF",
-      name: "cpfImage",
-      type: "file",
-    },
-
-    {
-      label: "Redes Sociais",
+      label: "Quais redes sociais da FURIA você já segue?",
       name: "socialMediasFollow",
       type: "button",
       options: [
@@ -67,6 +152,21 @@ export const FormMultiSteps = () => {
         "YouTube",
       ],
       validation: (value) => value.length > 0,
+    },
+    {
+      label: "Compartilhe suas redes sociais (Instagram e Twitter)",
+      name: "socialMediasOfUser",
+      type: "text",
+    },
+    {
+      label: "Em quais eventos da FURIA você participou no último ano?",
+      name: "eventsParticipationsLastYear",
+      type: "text",
+    },
+    {
+      label: "Você comprou produtos oficiais da FURIA no último ano? Quais?",
+      name: "buysLastYear",
+      type: "text",
     },
   ];
 
@@ -218,6 +318,153 @@ export const FormMultiSteps = () => {
                   </button>
                 ))}
               </div>
+            ) : currentStep.name === "buysLastYear" ? (
+              <div className="grid grid-cols-1 grid-rows-2 gap-4 mt-4">
+                {buysLastYear.map((product, index) => (
+                  <div key={index} className="flex gap-5 p-2 justify-center">
+                    <div className="flex gap-5">
+                      <input
+                        type="text"
+                        placeholder="Nome do Produto"
+                        className="w-[300px] h-[40px] px-2 py-1 border border-black rounded-lg text-black bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-black"
+                        onChange={(e) =>
+                          handleInputProductChange(
+                            index,
+                            "name",
+                            e.target.value
+                          )
+                        }
+                      />
+
+                      <input
+                        type="text"
+                        placeholder="Preço do Produto"
+                        className="w-[150px] h-[40px] px-2 py-1 border border-black rounded-lg text-black bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-black"
+                        value={product.price}
+                        onChange={(e) =>
+                          handleInputProductChange(
+                            index,
+                            "price",
+                            e.target.value
+                          )
+                        }
+                      />
+                    </div>
+
+                    <div className="flex gap-5">
+                      <input
+                        type="date"
+                        className="w-[200px] h-[40px] px-2 py-1 border border-black rounded-lg text-black bg-white focus:outline-none focus:ring-2 focus:ring-black"
+                        value={product.date}
+                        onChange={(e) =>
+                          handleInputProductChange(
+                            index,
+                            "date",
+                            e.target.value
+                          )
+                        }
+                      />
+
+                      <select
+                        name="typeProduct"
+                        className="w-[200px] h-[40px] px-2 py-1 border border-black rounded-lg text-black bg-white focus:outline-none focus:ring-2 focus:ring-black"
+                        value={product.typeProduct}
+                        onChange={(e) =>
+                          handleInputProductChange(
+                            index,
+                            "typeProduct",
+                            e.target.value
+                          )
+                        }
+                      >
+                        <option value="">Tipo do Produto</option>
+                        <option value="eletronic">Eletrônico</option>
+                        <option value="clothing">Vestuário</option>
+                        <option value="food">Alimento</option>
+                      </select>
+                    </div>
+                    <button
+                      type="button"
+                      onClick={() => handleRemoveProduct(index)}
+                      disabled={buysLastYear.length === 1}
+                      className="px-4 py-2 border border-red-600 text-red-600 bg-white rounded-md hover:bg-red-600 hover:text-white transition-colors"
+                    >
+                      Remover
+                    </button>
+                  </div>
+                ))}
+
+                <div className="flex gap-5 justify-center mt-4">
+                  <button
+                    type="button"
+                    onClick={handleAddProductInput}
+                    className="px-4 py-2 border border-black text-black bg-white rounded-md hover:bg-black hover:text-white transition-colors"
+                  >
+                    + Adicionar Produto
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={handleSaveProduct}
+                    className="px-4 py-2 border border-green-600 text-green-600 bg-white rounded-md hover:bg-green-600 hover:text-white transition-colors"
+                  >
+                    Salvar Produtos
+                  </button>
+                </div>
+              </div>
+            ) : currentStep.name === "eventsParticipationsLastYear" ? (
+              <div className="flex flex-col gap-4 mt-4">
+                {eventInputs.map((event, index) => (
+                  <div key={index} className="flex gap-5 p-2 justify-center">
+                    <input
+                      type="text"
+                      placeholder="Nome do Evento"
+                      className="w-[300px] h-[40px] px-2 py-1 border border-black rounded-lg text-black bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-black"
+                      value={event.name}
+                      onChange={(e) =>
+                        handleInputEventChange(index, "name", e.target.value)
+                      }
+                    />
+
+                    <input
+                      type="date"
+                      placeholder="Data do Evento"
+                      className="w-[200px] h-[40px] px-2 py-1 border border-black rounded-lg text-black bg-white focus:outline-none focus:ring-2 focus:ring-black"
+                      value={event.date}
+                      onChange={(e) =>
+                        handleInputEventChange(index, "date", e.target.value)
+                      }
+                    />
+
+                    <button
+                      type="button"
+                      onClick={() => handleRemoveEvent(index)}
+                      disabled={eventInputs.length === 1}
+                      className="px-4 py-2 border border-red-600 text-red-600 bg-white rounded-md hover:bg-red-600 hover:text-white transition-colors"
+                    >
+                      Remover
+                    </button>
+                  </div>
+                ))}
+
+                <div className="flex gap-5 justify-center mt-4">
+                  <button
+                    type="button"
+                    onClick={handleAddEventInput}
+                    className="px-4 py-2 border border-black text-black bg-white rounded-md hover:bg-black hover:text-white transition-colors"
+                  >
+                    + Adicionar Evento
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={handleSaveEvent}
+                    className="px-4 py-2 border border-green-600 text-green-600 bg-white rounded-md hover:bg-green-600 hover:text-white transition-colors"
+                  >
+                    Salvar Eventos
+                  </button>
+                </div>
+              </div>
             ) : currentStep.name === "cpfImage" ? (
               <div>
                 <input
@@ -226,6 +473,49 @@ export const FormMultiSteps = () => {
                   onChange={handleImageChange}
                   className="file-input"
                 />
+              </div>
+            ) : currentStep.name === "socialMediasOfUser" ? (
+              <div className="grid grid-rows-2">
+                <div className="flex flex-col">
+                  <label>Instagram</label>
+                  <input
+                    type="text"
+                    name="instagram"
+                    className="w-[500px] h-[50px] px-4 py-2 border border-black rounded-lg text-black bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-black"
+                    value={
+                      userData.socialMediasOfUser.instagram ||
+                      "https://instagram.com/"
+                    }
+                    placeholder="Coloque seu @ Aqui"
+                    onChange={(e) =>
+                      updateUserData({
+                        socialMediasOfUser: {
+                          ...userData.socialMediasOfUser,
+                          [e.target.name]: e.target.value,
+                        },
+                      })
+                    }
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label>Twitter</label>
+                  <input
+                    type="text"
+                    name="twitter"
+                    className="w-[500px] h-[50px] px-4 py-2 border border-black rounded-lg text-black bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-black"
+                    value={
+                      userData.socialMediasOfUser.twitter || "https://x.com/"
+                    }
+                    onChange={(e) =>
+                      updateUserData({
+                        socialMediasOfUser: {
+                          ...userData.socialMediasOfUser,
+                          [e.target.name]: e.target.value,
+                        },
+                      })
+                    }
+                  />
+                </div>
               </div>
             ) : currentStep.name === "address" ? (
               <div className="grid grid-cols-2 gap-4">
