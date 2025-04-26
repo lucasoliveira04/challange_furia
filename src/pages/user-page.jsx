0fd2b554cc9ced3c3d2b389d5933c2c248a88580
Lucas Oliveira Campos <lucasolisocialmedia@gmail.com>
2025-04-25 22:50:05 -0300

    Add UserPage tela de formulario onde o usuario passa seus dados

---
+++ src/pages/user-page.jsx
@@ -0,0 +1,9 @@
+import { FormMultiSteps } from "../components/form-multi-steps";
+
+export const UserPage = () => {
+  return (
+    <div className="w-full h-screen">
+      <FormMultiSteps />
+    </div>
+  );
+};
