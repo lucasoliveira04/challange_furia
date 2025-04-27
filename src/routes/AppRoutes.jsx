1049789e39e4355adf89485971dc74b57c1d176d
Lucas Oliveira Campos <lucasolisocialmedia@gmail.com>
2025-04-26 23:21:04 -0300

    feat: rota do resultado do formulario do usuario

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "../pages/home";
 import { UserPage } from "../pages/user-page";
+import { ResultCardUser } from "../pages/result-card-user";
 
 export const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ export const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/form" element={<UserPage />} />
+        <Route path="/result-card-user" element={<ResultCardUser />} />
 
         <Route path="*" element={<HomePage />} />
       </Routes>
